06f83d206dcf6b2c440a265272490e3a465e860b
breno.monte <breno.monte@unimedteresina.com.br>
2020-04-28 13:39:55 -0300

    - Creating buttons in grid

--- lib/game.dart
+++ lib/game.dart
@@ -6,6 +6,20 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
+
+  String _message = 'IT\'S TURN OF X';
+  IconData _currentIcon = Icons.clear;
+  int moves = 0;
+  bool playing = true;
+
+  List _blocks = [];
+
+  @override
+  void initState() {
+    _clearBlocks();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,63 +29,115 @@ class _GameState extends State<Game> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text('BM2'),
-            Text('TIC TAC TOE'),
+            Text('TIC TAC TOE', style: TextStyle(fontSize: 18),),
+            SizedBox(height: 20,),
+            Text(_message, style: TextStyle(fontSize: 34),),
             GridView.count(
                 crossAxisCount: 3,
                 shrinkWrap: true,
                 children: [
                   Container(
-                      child: Text('Item 1'),
+                      child: _blocks[0]['widget'],
                       decoration: _boxDecoration(right: true, bottom: true)
                   ),
                   Container(
-                      child: Text('Item 2'),
-                      decoration: _boxDecoration(right: true, bottom: true, left: true)
+                      child: _blocks[1]['widget'],
+                      decoration: _boxDecoration(right: true, bottom: true, left: true),
                   ),
-                  Container(
-                      child: Text('Item 3'),
-                      decoration: _boxDecoration(left: true, bottom: true)
+                  InkWell(
+                    child: Container(
+                        child: _blocks[2]['widget'],
+                        decoration: _boxDecoration(left: true, bottom: true)
+                    ),
+                    onTap: (){
+
+                    },
                   ),
 
                   Container(
-                      child: Text('Item 4'),
+                      child: _blocks[3]['widget'],
                       decoration: _boxDecoration(right: true, bottom: true, top: true)
                   ),
                   Container(
-                      child: Text('Item 5'),
+                      child: _blocks[4]['widget'],
                       decoration: _boxDecoration(right: true, bottom: true, top: true, left: true)
                   ),
                   Container(
-                      child: Text('Item 6'),
+                      child: _blocks[5]['widget'],
                       decoration: _boxDecoration(bottom: true, top: true, left: true)
                   ),
 
                   Container(
-                      child: Text('Item 7'),
+                      child: _blocks[6]['widget'],
                       decoration: _boxDecoration(right: true, top: true)
                   ),
                   Container(
-                      child: Text('Item 8'),
+                      child: _blocks[7]['widget'],
                       decoration: _boxDecoration(right: true, top: true, left: true)
                   ),
                   Container(
-                      child: Text('Item 9'),
+                      child: _blocks[8]['widget'],
                       decoration: _boxDecoration(top: true, left: true)
                   ),
                 ]
             ),
-            SizedBox(height: 40),
+            SizedBox(height: 30),
             FloatingActionButton.extended(
               label: Text('RESET', style: TextStyle(color: Colors.white),),
               icon: Icon(Icons.refresh, color: Colors.white,),
               onPressed: (){
-                print('reset game');
+                _clearBlocks();
               },
             ),
           ],
         )
       ),
     );
+  }
+
+  _changeBlock(int number){
+    setState(() {
+      if(playing){
+        moves++;
+        _blocks[number]['widget'] = Center(child: Icon(_currentIcon, size: 80));
+        if(_currentIcon == Icons.clear){
+          _currentIcon = Icons.panorama_fish_eye;
+          _blocks[number]['value'] = 0;
+          _message = 'IT\'S TURN OF O';
+        } else {
+          _blocks[number]['value'] = 1;
+          _currentIcon = Icons.clear;
+          _message = 'IT\'S TURN OF X';
+        }
+      }
+    });
+
+    if(moves >= 5) {
+      _checkWinner();
+    }
+  }
+
+  _checkWinner(){
+    //TODO: check if there is a Winner
+    //If moves = 9 and there's no Winner, its draw!
+  }
+
+  _clearBlocks(){
+    setState(() {
+      _blocks = [
+        {'widget': InkWell(onTap: (){ _changeBlock(0); }), 'value': -1},
+        {'widget': InkWell(onTap: (){ _changeBlock(1); }), 'value': -1},
+        {'widget': InkWell(onTap: (){ _changeBlock(2); }), 'value': -1},
+
+        {'widget': InkWell(onTap: (){ _changeBlock(3); }), 'value': -1},
+        {'widget': InkWell(onTap: (){ _changeBlock(4); }), 'value': -1},
+        {'widget': InkWell(onTap: (){ _changeBlock(5); }), 'value': -1},
+
+        {'widget': InkWell(onTap: (){ _changeBlock(6); }), 'value': -1},
+        {'widget': InkWell(onTap: (){ _changeBlock(7); }), 'value': -1},
+        {'widget': InkWell(onTap: (){ _changeBlock(8); }), 'value': -1},
+      ];
+    });
   }
 
   _boxDecoration({left: false, top: false, right: false, bottom: false}){
